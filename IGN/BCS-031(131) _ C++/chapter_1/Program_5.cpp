6ec376e03356ed14e4be8038c4a034d2a8dd3cb0
ABHAY KUMAR <139012978+kumarabhay1029@users.noreply.github.com>
2025-11-22 02:48:25 +0000

    try and added new programs

---
+++ IGN/BCS-031(131) _ C++/chapter_1/Program_5.cpp
@@ -0,0 +1,13 @@
+//myfirst string
+#include <iostream>
+#include <string>
+using namespace std;
+int main ()
+{
+    string mystring;
+    mystring = "This is my first string content";
+    cout << mystring << endl;
+    mystring = "This is different String Content";
+    cout << mystring << endl;
+    return 0;
+}
---
+++ IGN/BCS-031(131) _ C++/chapter_1/Program_6.cpp
@@ -0,0 +1,14 @@
+// defined constants, calculate circleference
+#include <iostream>
+using namespace std;
+#define PI 3.1415
+#define NEWLINE '\n'
+int main()
+{
+    double r, circle;
+    r = 5.0;
+    circle = 2*PI*r;
+    cout << circle;
+    cout << NEWLINE;
+    return 0;
+}
